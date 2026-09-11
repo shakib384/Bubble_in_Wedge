fdad8a9c232ea60d53728f9a93b56a8087c4cdd4
shakib384 <113724323+shakib384@users.noreply.github.com>
2026-09-10 20:57:46 -0400

    Update makemovie.sh

--- out/makemovie.sh
+++ out/makemovie.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
 
+ffmpeg -y -framerate 40 -pattern_type glob -i 'vof-omega_y_view1_*.png' -c:v libx264 -crf 18 -pix_fmt yuv420p vof-omega_y_view1.mp4
+ffmpeg -y -framerate 40 -pattern_type glob -i 'vof-p_view1_*.png' -c:v libx264 -crf 18 -pix_fmt yuv420p vof-p_view1.mp4
+ffmpeg -y -framerate 40 -pattern_type glob -i 'vof-p_view2_*.png' -c:v libx264 -crf 18 -pix_fmt yuv420p vof-p_view2.mp4
+ffmpeg -y -framerate 40 -pattern_type glob -i 'vof-ux_view1_*.png' -c:v libx264 -crf 18 -pix_fmt yuv420p vof-ux_view1.mp4
+ffmpeg -y -framerate 40 -pattern_type glob -i 'vof-uz_view1_*.png' -c:v libx264 -crf 18 -pix_fmt yuv420p vof-uz_view1.mp4
+ffmpeg -y -framerate 40 -pattern_type glob -i 'vof-uy_view4_*.png' -c:v libx264 -crf 18 -pix_fmt yuv420p vof-uy_view4.mp4
+ffmpeg -y -framerate 40 -pattern_type glob -i 'vof-uz_view4_*.png' -c:v libx264 -crf 18 -pix_fmt yuv420p vof-uz_view4.mp4
+ffmpeg -y -framerate 40 -pattern_type glob -i 'vof-p_view4_*.png' -c:v libx264 -crf 18 -pix_fmt yuv420p vof-p_view4.mp4
